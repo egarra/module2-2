137339e9d5314429a3a6b885f38a9fccec0e5b41
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-15 16:24:28 +0200

    add delete and filter methods

--- src/Tabs/Todos/Todos.jsx
+++ src/Tabs/Todos/Todos.jsx
@@ -2,10 +2,11 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { SearchForm } from 'components/SearchForm/SearchForm';
 import { ToDoList } from 'components/ToDoList/ToDoList';
-
+import { Filter } from 'components/Filter/Filter';
 export class Todos extends Component {
   state = {
     todos: [],
+    filter: '',
   };
 
   onFormSubmit = text => {
@@ -15,12 +16,28 @@ export class Todos extends Component {
     };
     this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
   };
-
+  deleteToDo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== id),
+    }));
+  };
+  onInputChange = filter => {
+    this.setState({
+      filter 
+    })
+  };
+  filteredToDos = () => { 
+    const { todos, filter } = this.state;
+    return todos.filter(todo => todo.text.includes(filter));
+  }
   render() {
+    const filteredToDos = this.filteredToDos();
     return (
       <>
         <SearchForm onFormSubmit={this.onFormSubmit} btnText="Create" />
-        <ToDoList data={this.state.todos} />
+        <p>Enter your search value</p>
+        <Filter onInputChange={this.onInputChange } />
+        <ToDoList data={filteredToDos} deleteToDo={this.deleteToDo} />
       </>
     );
   }
