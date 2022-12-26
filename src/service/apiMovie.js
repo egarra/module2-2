a49f030336371df76a0a6b29c50b33e25efb91e8
Volodymyr Babych <v.d.babych@gmail.com>
2022-12-26 15:53:23 +0200

    add hooks to Movies

--- src/service/apiMovie.js
+++ src/service/apiMovie.js
@@ -1,24 +1,24 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-// axios.defaults.params = {
-//   api_key: '5ba51c2da469b356ab2a1378773a169b',
-// };
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+axios.defaults.params = {
+  api_key: '5ba51c2da469b356ab2a1378773a169b',
+};
 
-// export const getTrendingMovie = async () => {
-//   const { data } = await axios.get('/trending/all/day', {
-//     params: {
-//       page: 1,
-//     },
-//   });
-//   return data;
-// };
+export const getTrendingMovie = async () => {
+  const { data } = await axios.get('/trending/all/day', {
+    params: {
+      page: 1,
+    },
+  });
+  return data;
+};
 
-// export const getMovieByQuery = async query => {
-//   const { data } = await axios.get('/search/movie', {
-//     params: {
-//       query,
-//     },
-//   });
-//   return data;
-// };
+export const getMovieByQuery = async query => {
+  const { data } = await axios.get('/search/movie', {
+    params: {
+      query,
+    },
+  });
+  return data;
+};
