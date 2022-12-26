9fda72996935c6750bae82fc3f025358d8dbad4c
Svitlana Synytsia <svetlankogr@gmail.com>
2022-12-26 14:06:56 +0100

    task 13

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,11 @@
-export const FriendList = ({ friends }) => {
+export const FriendList = ({ friends, deleteFriend }) => {
   return (
     <ul>
-      {friends.map(({ name, email, phone, online }) => {
+      {friends.map(({ name, email, phone, online, id }) => {
         const mailTo = 'mailto:' + email;
         const tel = 'tel' + phone;
         return (
-          <li>
+          <li key={name}>
             <h3>{name}</h3>
             <a href={mailTo}>{email}</a>
             <a href={tel}>{phone}</a>
@@ -17,6 +17,9 @@ export const FriendList = ({ friends }) => {
                 display: 'block',
               }}
             ></span>
+            <button type="button" onClick={() => deleteFriend(id)}>
+              Delete
+            </button>
           </li>
         );
       })}
