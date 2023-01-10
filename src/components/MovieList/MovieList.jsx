d8b0c46859b039f132a6226b10712f33d946e5d1
Svitlana Synytsia <svetlankogr@gmail.com>
2023-01-10 14:04:05 +0100

    go-back

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const MovieList = ({ movies }) => {
+  const location = useLocation();
+
   return (
     <ul>
       {movies.map(({ poster_path, title, id }) => (
         <li key={id}>
-          <Link to={`/movies/${id}`}>
-          <h2>{title}</h2>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-            alt={title}
-          />
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            <h2>{title}</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+              alt={title}
+            />
           </Link>
         </li>
       ))}
