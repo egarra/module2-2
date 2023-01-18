1286cd26f4aa7055941b7178e02030c07d0e8360
Svitlana Synytsia <svetlankogr@gmail.com>
2023-01-18 13:51:03 +0100

    addTodo

--- src/redux/todos/operations.js
+++ src/redux/todos/operations.js
@@ -1,18 +1,30 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const instanceTodos = axios.create({
-    baseURL: 'https://63c683234ebaa80285476130.mockapi.io/',
+  baseURL: 'https://63c683234ebaa80285476130.mockapi.io/',
 });
 
 export const fetchTodos = createAsyncThunk(
-    "todos/fetchTodos",
-    async (_, thunkAPI) => {
-        try {
-            const response = await instanceTodos.get("/todos");
-            return response.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
+  'todos/fetchTodos',
+  async (_, thunkAPI) => {
+    try {
+      const response = await instanceTodos.get('/todos');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
+  }
+);
+
+export const addTodo = createAsyncThunk(
+  'todos/addTodo',
+  async (todo, thunkAPI) => {
+    try {
+      const response = await instanceTodos.post('/todos', todo);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
 );
