1488e70b3afe5fe9b3fe36082a3b0f51032a6f88
AlexArmor <lexlutermotion@gmail.com>
2023-01-19 15:11:20 +0200

    friends api

--- src/redux/friends/friendsSlice.js
+++ src/redux/friends/friendsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchFriends } from './operations';
+import { fetchFriends, deleteFriends } from './operations';
 
 const friendsInitialState = {
   items: [],
@@ -10,11 +10,34 @@ const friendsInitialState = {
 const friendsSlice = createSlice({
   name: 'friends',
   initialState: friendsInitialState,
-  extraReducers: builder =>
-    builder
-      .addCase(fetchFriends.fulfilled, (state, { payload }) => {
-        state.items = [...payload].reverse();
-      }),
+  extraReducers: {
+    [fetchFriends.pending](state) {
+      state.isLoading = true;
+    },
+    [fetchFriends.fulfilled](state, { payload }) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = [...payload].reverse();
+    },
+    [fetchFriends.rejected](state, { payload }) {
+      state.isLoading = false;
+      state.error = payload;
+    },
+
+    [deleteFriends.pending](state) {
+      state.isLoading = true;
+    },
+    [deleteFriends.fulfilled](state, { payload }) {
+      state.isLoading = false;
+      state.error = null;
+      console.log('payload', payload);
+      state.items = state.items.filter(item => item.id !== payload.id);
+    },
+    [deleteFriends.rejected](state, { payload }) {
+      state.isLoading = false;
+      state.error = payload;
+    },
+  },
 });
 
 export const friendsReducer = friendsSlice.reducer;
